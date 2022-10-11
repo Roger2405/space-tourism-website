036a53a57d5b3c885b7fc76c130b311da23c103e
Roger <rogerdr.rdr@gmail.com>
2022-10-10 21:54:33 -0300

    layout desktop home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,15 +8,17 @@ export default function Home() {
 
     return (
         <div className={Styles.home}>
-            <h1 className={Styles.titleHome}>
-                So, you want to travel to
-                <br /><b>Space</b>
-            </h1>
-            <TextContainer>
-                Let’s face it; if you want to go to space, you might as well genuinely go to
-                outer space and not hover kind of on the edge of it.Well sit back, and relax
-                because we’ll give you a truly out of this world experience!
-            </TextContainer>
+            <div className={Styles.content}>
+                <h1 className={Styles.titleHome}>
+                    So, you want to travel to
+                    <br /><b>Space</b>
+                </h1>
+                <TextContainer>
+                    Let’s face it; if you want to go to space, you might as well genuinely go to
+                    outer space and not hover kind of on the edge of it.Well sit back, and relax
+                    because we’ll give you a truly out of this world experience!
+                </TextContainer>
+            </div>
             <div className={Styles.link}>
                 <Link className={Styles.button} to={'/destination'} ><span>Explore</span></Link>
 
